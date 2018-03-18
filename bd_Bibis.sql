5795d36473e14c96d4b068b906ccfca638a66cae
DanielFrancoOrtiz <s14120049@alumnos.itsur.edu.mx>
2018-03-18 09:38:33 -0600

    Se modificaron detalles de la base de datos

--- bd_Bibis.sql
+++ bd_Bibis.sql
@@ -1,44 +1,45 @@
 
+
 create database bd_Bibis;
 use bd_Bibis;
 
 create table empleados(
 id			int 		primary key auto_increment,
-nombre		varchar(30)	not null,
-apellidos	varchar(50) not null,
-usuario		varchar(30) not null,
-puesto		varchar(30) not null,
-email		varchar(50) null,
-telefono	varchar(16) null,
-direccion	text		not null,
-pass		varchar(30) not null
+Nombre		varchar(30)	not null,
+Apellidos	varchar(50) not null,
+Usuario		varchar(30) not null,
+Puesto		varchar(30) not null,
+Email		varchar(50) null,
+Telefono	varchar(16) null,
+Direccion	text		not null,
+Pass		varchar(30) not null
 );
 
 create table clientes(
 id				int 		primary key auto_increment,
-nombre			varchar(30)	not null,
-apellidos		varchar(50) not null,
-email			varchar(50) not null,
-telefono		varchar(16) not null,
-direccion		text		not null,
-ciudad			varchar(30) not null,
-codigo_postal	char(5)		not null
+Nombre			varchar(30)	not null,
+Apellidos		varchar(50) not null,
+Email			varchar(50) not null,
+Telefono		varchar(16) not null,
+Direccion		text		not null,
+Ciudad			varchar(30) not null,
+Codigo_postal	char(5)		not null
 
 );
 
 create table catergorias(
 id			int 		primary key auto_increment,
-nombre		varchar(30)	not null,
-descripcion	text null
+Nombre		varchar(30)	not null,
+Descripcion	text null
 );
 
 create table proveedores(
 id					int			primary key auto_increment,
-nombre_proveedor  	varchar(50)	not null,
-email_proveedor		varchar(50)	not null,
-telefono_proveedor	varchar(16)	not null,
-direccion_proveedor	text		not null,
-ciudad_proveedor	varchar(30)	not null
+Nombre 			 	varchar(50)	not null,
+Email				varchar(50)	not null,
+Telefono			varchar(16)	not null,
+Direccion			text		not null,
+Ciudad				varchar(30)	not null
 );
 
 
@@ -58,18 +59,18 @@ constraint foreign key (id_Categoria) references catergorias(id)
 
 create table mensajeria(
 id					int 			primary key auto_increment,
-nombre_mensajeria	varchar(50) 	not null,
-email_mensajeria	varchar(50)		not null,
-telefono_mensajeria	varchar(16)		not null
+Nombre				varchar(50) 	not null,
+Email				varchar(50)		not null,
+Telefono			varchar(16)		not null
 );
 
 create table ordenes (
 id				int 			primary key auto_increment,
-fecha_envio		date			not null,
-fecha_entrega	date			not null,
-ciudad			varchar(30)		not null,
-direccion		text			not null,
-precio_envio	decimal(10,2)	not null,
+Fecha_envio		date			not null,
+Fecha_entrega	date			not null,
+Ciudad			varchar(30)		not null,
+Direccion		text			not null,
+Precio_envio	decimal(10,2)	not null,
 id_Cliente		int 			not null,
 id_Mensajeria	int				not null,	
 constraint foreign key(id_Cliente) references clientes(id),
@@ -84,6 +85,8 @@ id_orden		int				not null,
 constraint	foreign key(id_orden)references	ordenes(id)
 );
 
+select * from proveedores;
+insert into proveedores values(null,'proveedor1','pro1@email.com','4434534352','pro1direccion','pro1city');
 
 
 
