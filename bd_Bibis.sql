eb479020c27083d483bfee11365268840becb379
DanielFrancoOrtiz <s14120049@alumnos.itsur.edu.mx>
2018-05-21 17:28:23 -0500

    Se modifico la base de datos para eliminar la tabla empleados

--- bd_Bibis.sql
+++ bd_Bibis.sql
@@ -1,23 +1,6 @@
-
-
 create database bd_Bibis;
 use bd_Bibis;
 
-create table empleados(
-id			int 		primary key auto_increment,
-Nombre		varchar(30)	not null,
-Apellidos	varchar(50) not null,
-Usuario		varchar(30) not null,
-Puesto		varchar(30) not null,
-Email		varchar(50) null,
-Telefono	varchar(16) null,
-Direccion	text		not null,
-Pass		varchar(30) not null
-);
-
-create view empleadosv as 
-select Nombre, Apellidos, Puesto, Email, Telefono, Direccion from
-empleados;
 
 
 create table clientes(
@@ -103,9 +86,6 @@ insert into categorias (nombre, descripcion) values('Categoria3','descripcion ca
 insert into categorias (nombre, descripcion) values('Categoria4','descripcion categoria 4');
 
 
-insert into Empleados(Nombre, Apellidos, Usuario, Puesto, Email,Telefono, Direccion, Pass)
-				values('Empleado1', 'Apellidos 1', 'user1','Puesto',  'email.user1@bibid.com.mx',
-                '4451211549','direccion', 'pass');
 
 insert into clientes(Nombre, Apellidos, Email, Telefono, Direccion, Ciudad, Codigo_Postal)
 				values('Cliente 1','Apellidos', 'email.cliente@email.com',
