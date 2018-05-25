26b53734efe8fc4a7c4be533b0c01c865f553539
DanielFrancoOrtiz <s14120049@alumnos.itsur.edu.mx>
2018-05-24 22:49:44 -0500

    Se modifico la base de datos en el campo pass para aceptar el encriptado de tipo sha1

--- bd_empleados_bibis.sql
+++ bd_empleados_bibis.sql
@@ -11,7 +11,7 @@ Puesto		varchar(30) not null,
 Email		varchar(50) null,
 Telefono	varchar(16) null,
 Direccion	text		not null,
-Pass		varchar(30) not null
+Pass		varchar(50) not null
 );
 
 create view empleadosv as 
@@ -21,4 +21,8 @@ empleados;
 
 insert into Empleados(Nombre, Apellidos, Usuario, Puesto, Email,Telefono, Direccion, Pass)
 				values('Empleado1', 'Apellidos 1', 'user1','Puesto',  'email.user1@bibid.com.mx',
-                '4451211549','direccion', 'pass');
+                '4451211549','direccion', sha1('pass'));
+select * from empleados;
+
+
+
