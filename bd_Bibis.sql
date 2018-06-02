b869d157b61ea45a9cbc122264131d9b2b4a10a9
DanielFrancoOrtiz <s14120049@alumnos.itsur.edu.mx>
2018-06-01 22:41:10 -0500

    Se agregaron las tablas de preventa y detalleventa

--- bd_Bibis.sql
+++ bd_Bibis.sql
@@ -104,4 +104,35 @@ insert into Mensajeria(Nombre,Email,Telefono)
             
 -- insert into detaller_ordenes (precio, cantidad, id_orden)values();
 
+use bd_bibis;
 
+-- ---------------------------------------- CARRITO DE VENTA
+create table pre_venta
+(
+id_Pventa	int primary key auto_increment,
+nombreP		varchar(60) not null,
+precioV		double not null
+);
+
+create table detalles_venta
+(
+id_Dventa int primary key auto_increment,
+cantidadP	int not null,
+precioT		double not null,
+id_Pventa	int,
+constraint foreign key (id_Pventa) references pre_venta(id_Pventa)
+);
+
+select * from productos;
+
+-- Agregar al carrito
+insert into pre_venta values (null, (select nombre from productos where nombre='Perjum'), (select Precio_venta from productos where nombre='Perjum'));
+
+
+select * from pre_venta;
+-- Total de productos y total de precio
+insert into detalles_venta values (null, (select count(*) from pre_venta), (select sum(precioV) from pre_venta), null);
+
+
+select * from detalles_venta;
+show databases;
